2c24419a7c773a8d2bd4e2442d37a5b86df6a579
JeffLin0225 <jefflin0225@gmail.com>
2024-07-14 13:10:43 +0800

    first commit

---
+++ spingbootCloth/src/main/java/com/cloth/model/Customerservice.java
@@ -0,0 +1,55 @@
+package com.cloth.model;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name="customerservice")
+public class Customerservice {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "users_id")
+	private Users users;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "admin_id")
+	private Admin admin;
+	
+	private String chats;
+	@Lob
+	private byte[] img1;
+	@Lob
+	private byte[] img2;
+	@Lob
+	private byte[] img3;
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss EEEE")
+	@Temporal(TemporalType.TIMESTAMP) 
+	private Date send_at;
+
+}
