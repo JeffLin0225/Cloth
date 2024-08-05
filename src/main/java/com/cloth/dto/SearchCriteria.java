db4f639f84780e3cbf0111523d5382095a7cdf5d
anita1 <cat29127743@gmail.com>
2024-08-05 21:38:09 +0800

    08/5 9.38

---
+++ src/main/java/com/cloth/dto/SearchCriteria.java
@@ -0,0 +1,21 @@
+package com.cloth.dto;
+
+
+
+
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class SearchCriteria {
+
+    private String size;
+    private String style;
+    private String color;
+	
+}
